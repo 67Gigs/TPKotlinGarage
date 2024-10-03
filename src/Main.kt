182708e4c7a17d54b7ceadfb9424558ba17c400f
noureddine.bensadok@ouidou.fr <noureddine.bensadok@ouidou.fr>
2024-10-03 15:47:43 +0200

    Implementation de nouvelles fonctionnalité

--- src/Main.kt
+++ src/Main.kt
@@ -5,17 +5,25 @@ import classes.Voiture
 
 fun main() {
 
-    val voiture = Voiture("Peugeot", "Gris", 2019, 4, 8)
-    val moto = Moto("Yamaha", "Noir", 2020, true)
-    val camion = Camion("Renault", "Blanc", 2018,  123.0)
-
     val garage = Garage(arrayListOf())
 
-    garage.ajouterVehicule(voiture)
-    garage.ajouterVehicule(moto)
-    garage.ajouterVehicule(camion)
+    garage.ajouterVehicule(Voiture("Peugeot", "Gris", 2019, 4, 8))
+    garage.ajouterVehicule(Voiture("Renault", "Bleu", 2018, 4, 5))
+    garage.ajouterVehicule(Voiture("Porsche 911", "Vert", 2020, 4, 6))
+
+    garage.ajouterVehicule(Moto("Honda", "Rouge", 2017, false))
+    garage.ajouterVehicule(Moto("Yamaha", "Noir", 2020, true))
+
+    garage.ajouterVehicule(Camion("Renault", "Blanc", 2018,  123.0))
+    garage.ajouterVehicule(Camion("Mercedes", "Jaune", 2019,  110.0))
 
     garage.afficherGarage()
 
-    for (vehicule in garage.vehicules) { vehicule.klaxonner() }
+    garage.klaxonner()
+
+    garage.affichageChoix("Voiture")
+
+    garage.affichageChoix("Moto")
+
+    garage.affichageChoix("Camion")
 }
--- src/classes/Garage.kt
+++ src/classes/Garage.kt
@@ -2,13 +2,60 @@ package classes
 
 data class Garage(var vehicules: ArrayList<Vehicule>) {
 
-    fun ajouterVehicule(vehicule: Vehicule) {
+    public fun ajouterVehicule(vehicule: Vehicule) {
         vehicules += vehicule
     }
 
-    fun afficherGarage() {
+    public fun afficherGarage() {
         for (vehicule in vehicules) {
             vehicule.afficherDetails()
+        }
+    }
+
+    public fun afficherCamions() {
+        for (vehicule in vehicules) {
+            if (vehicule is Camion) {
+                vehicule.afficherDetails()
+            }
+        }
+    }
+
+    public fun affichageMotos() {
+        for (vehicule in vehicules) {
+            if (vehicule is Moto) {
+                vehicule.afficherDetails()
+            }
+        }
+    }
+
+    public fun afficherVoitures() {
+        for (vehicule in vehicules) {
+            if (vehicule is Voiture) {
+                vehicule.afficherDetails()
+            }
+        }
+    }
+
+    public fun affichageChoix(choix: String) {
+        when (choix.uppercase()) {
+            "Voiture".uppercase() -> afficherVoitures()
+            "Moto".uppercase() -> affichageMotos()
+            "Camion".uppercase() -> afficherCamions()
+            else -> afficherGarage()
+        }
+    }
+
+    public fun affichageOrdre(choix: String) {
+        when (choix.uppercase()) {
+            "Marque".uppercase() -> vehicules.sortBy { it.marque }
+            "Annee".uppercase() -> vehicules.sortBy { it.annee }
+            else -> vehicules.sortBy { it.marque }
+        }
+    }
+
+    public fun klaxonner() {
+        for (vehicule in vehicules) {
+            vehicule.klaxonner()
         }
     }
 }
